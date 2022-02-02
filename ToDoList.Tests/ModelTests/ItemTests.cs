ae0bc57e0b03c41c272319efe6deb1eac1716595
marnionrails <37088724+marnionrails@users.noreply.github.com>
2022-02-02 17:10:48 -0500

    pass test for getting item description

--- ToDoList.Tests/ModelTests/ItemTests.cs
+++ ToDoList.Tests/ModelTests/ItemTests.cs
@@ -9,9 +9,16 @@ namespace ToDoList.Tests
     [TestMethod]
     public void ItemConstructor_CreatesInstanceOfItem_Item()
     {
-      Item newItem = new Item();
+      Item newItem = new Item("test");
       Assert.AreEqual(typeof(Item), newItem.GetType());
     }
-
+    [TestMethod]
+      public void GetDescription_ReturnsDescription_String()
+    {
+      string description = "Clean the porch.";
+      Item newItem = new Item(description);
+      string result = newItem.Description;
+      Assert.AreEqual(description, result);
+    }
   }
 }
